f9dde5534afbc31a4181dfd6a967aa6dccdc08cb
Oleg <Oleg-BLR@yandex.ru>
2022-11-03 13:34:48 +0300

    Solution 01

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,26 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String colour, int numberOfPaws, boolean hasFur) {
+        this.color = colour;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        if (hasFur == true) {
+            if (numberOfPaws == 1) {
+                return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and a fur.";
+            }
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and a fur.";
+        } else if (numberOfPaws > 1) {
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and no fur.";
+        }
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and no fur.";
+    }
+
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
 
+    public Dog(String colour, int numberOfPaws, boolean hasFur) {
+        super(colour = "brown", numberOfPaws = 4, hasFur = true);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
 }
